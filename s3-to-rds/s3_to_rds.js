9aa6c04c8b109d7cd7adfd5685fa0ca04a62b7fd
David Zwarg <dzwarg@azavea.com>
2018-05-01 17:50:05 +0000

    RDS resources, env vars, and pg-client.

--- s3-to-rds/s3_to_rds.js
+++ s3-to-rds/s3_to_rds.js
@@ -1,7 +1,33 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const { Pool, Client } = require('pg')
+
+const connectionString = 'postgresql://dbuser:secretpassword@database.server.com:3211/mydb'
 
 module.exports.handler = (event, context, callback) => {
-    callback({'message':'Not implemented'});
+    console.log(`host: ${process.env.POSTGRES_HOST}`)
+    console.log(`user: ${process.env.POSTGRES_USER}`)
+    console.log(`db  : ${process.env.POSTGRES_DATABASE}`)
+    
+    const client = new Client({
+      user: 'dbuser',
+      host: 'database.server.com',
+      database: 'mydb',
+      password: 'secretpassword',
+      port: 3211,
+    });
+    
+    client.connect();
+    
+    client.query('SELECT NOW()', (err, res) => {
+        if (err) {
+            console.log(err);
+            callback(err);
+        }
+        
+        console.log(null, res);
+        client.end();
+    });
+    
 };
