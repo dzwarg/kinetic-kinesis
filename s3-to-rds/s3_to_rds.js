41e6a5a48c3972edf9b062a8ee989e58db20264e
David Zwarg <dzwarg@azavea.com>
2018-05-02 02:45:13 +0000

    Use callback from client.connect()

--- s3-to-rds/s3_to_rds.js
+++ s3-to-rds/s3_to_rds.js
@@ -1,21 +1,26 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-const { Pool, Client } = require('pg')
+const {Client} = require('pg')
 
 module.exports.handler = (event, context, callback) => {
     const client = new Client();
     
-    client.connect();
-    
-    client.query('SELECT NOW()', (err, res) => {
+    client.connect((err) => {
         if (err) {
             console.log(err);
             callback(err);
+            return;
         }
         
-        console.log(null, res);
-        client.end();
+        client.query('SELECT NOW()', (err, res) => {
+            if (err) {
+                console.log(err);
+                callback(err);
+            }
+            
+            console.log(null, res);
+            client.end();
+        });
     });
-    
 };
