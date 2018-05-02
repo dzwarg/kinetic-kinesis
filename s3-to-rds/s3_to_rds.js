eed85e76f58898c0d97dfa1d54271cee68476c9a
David Zwarg <dzwarg@azavea.com>
2018-05-02 05:07:16 +0000

    Add some RDS logic to store stuff.

--- s3-to-rds/s3_to_rds.js
+++ s3-to-rds/s3_to_rds.js
@@ -3,24 +3,49 @@
 const AWS = require('aws-sdk');
 const {Client} = require('pg')
 
+const createTableText = `CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS content (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  data JSONB
+);`
+
+const ensureSchema = (client) => {
+    return client.query(createTableText)
+}
+
+const errorReporter = (callback) => (err) => {
+    console.log(err);
+    callback(err);
+}
+
+const insertContent = (client, event) => {
+    return client.query('SELECT NOW()');
+}
+
 module.exports.handler = (event, context, callback) => {
     const client = new Client();
+    const errorCallback = errorReporter(callback);
     
     client.connect((err) => {
         if (err) {
-            console.log(err);
-            callback(err);
+            errorCallback(err);
             return;
         }
         
-        client.query('SELECT NOW()', (err, res) => {
-            if (err) {
-                console.log(err);
-                callback(err);
-            }
-            
-            console.log(null, res);
-            client.end();
-        });
+        ensureSchema(client)
+            .then(
+                insertContent(event)
+                    .then((res) => {
+                        console.log(JSON.stringify(res))
+                        callback(null, res);
+                    })
+                    .catch(errorCallback)
+            )
+            .catch(errorCallback)
+            .then(() => {
+                console.log({'message': 'Complete'})
+                client.end();
+            });
     });
 };
